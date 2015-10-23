a42499ccc83ab835244229b18e8472111eb5457a
IdcNoob <zajabli@gmail.com>
2015-10-23 13:29:06 +0300

    fixes

--- CourierBottleAbuse/Program.cs
+++ CourierBottleAbuse/Program.cs
@@ -7,41 +7,41 @@ using Ensage.Common.Extensions;
 namespace CourierBottleAbuse {
 	class Program {
 
-		private const int WM_KEYUP = 0x0101;
-
 		private static bool _enabled = false;
 		private static bool _following = false;
 
 		private static void Main() {
-			Game.OnUpdate += Game_OnUpdate;
+			Game.OnIngameUpdate += Game_OnIngameUpdate;
 			Game.OnWndProc += Game_OnWndProc;
 		}
+
 		private static void Game_OnWndProc(WndEventArgs args) {
-			if (args.WParam == 'Z' && !Game.IsChatOpen && args.Msg == WM_KEYUP) {
+			if (args.WParam == 'Z' && !Game.IsChatOpen && args.Msg == (uint) Utils.WindowsMessages.WM_KEYUP) {
 				_enabled = true;
 				_following = false;
 			}
 		}
 
-		private static void Game_OnUpdate(EventArgs args) {
-
-			if (!Game.IsInGame || !_enabled || Game.IsPaused || !Utils.SleepCheck("delay"))
-				return;
-
-			var player = ObjectMgr.LocalPlayer;
-			if (player == null || player.Team == Team.Observer)
+		private static void Game_OnIngameUpdate(EventArgs args) {
+			if (Game.IsPaused || !_enabled || !Utils.SleepCheck("delay"))
 				return;
 
 			var hero = ObjectMgr.LocalHero;
 
+			if (hero == null || !hero.IsAlive) {
+				_enabled = false;
+				return;
+			}
+
 			var bottle = hero.FindItem("item_bottle");
-			var courier = ObjectMgr.GetEntities<Courier>().FirstOrDefault();
+			var courier = ObjectMgr.GetEntities<Courier>().FirstOrDefault(x => x.IsAlive && x.Team == hero.Team);
 			var courBottle = courier.FindItem("item_bottle");
 
 			if ((bottle == null && courBottle == null) || courier == null) {
 				_enabled = false;
 				return;
 			}
+
 			var distance = hero.Distance2D(courier);
 
 			if (distance > 200 && !_following) {
@@ -49,18 +49,17 @@ namespace CourierBottleAbuse {
 				_following = true;
 			}
 
-			if (distance <= 200 && _following) {
+			if (distance <= 200 && _following/* && bottle != null && bottle.CurrentCharges == 0*/) {
 				hero.Stop();
 				hero.GiveItem(bottle, courier);
 				_following = false;
 			}
 
 			if (distance <= 200 && !_following && courBottle != null) {
-				courier.FindSpell("courier_return_to_base").UseAbility();
-				if (courier.IsFlying) courier.FindSpell("courier_burst").UseAbility();
-				courier.FindSpell("courier_take_stash_items").UseAbility(true);
-				courier.FindSpell("courier_transfer_items").UseAbility(true);
-
+				courier.Spellbook.SpellQ.UseAbility();
+				if (courier.IsFlying) courier.Spellbook.SpellR.UseAbility();
+				courier.Spellbook.SpellD.UseAbility(true);
+				courier.Spellbook.SpellF.UseAbility(true);
 				_enabled = false;
 			}
 
--- HpMpAbuse/Program.cs
+++ HpMpAbuse/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Ensage;
 using Ensage.Common;
@@ -11,13 +12,17 @@ namespace HpMpAbuse {
 
 		private static bool _stopAttack = true;
 
-		private static int _lastPtState = 0;
+		private static Ensage.Attribute _lastPtState;
 		private static bool _ptChanged = false;
-	
+
+		private static readonly string[] bonusHealth = { "bonus_strength", "bonus_all_stats", "bonus_health" };
+		private static readonly string[] bonusMana = { "bonus_intellect", "bonus_all_stats", "bonus_mana" };
+
 		private static void Main() {
-			Game.OnUpdate += Game_OnUpdate;
+			Game.OnIngameUpdate += Game_OnIngameUpdate;
 			Game.OnWndProc += Game_OnWndProc;
 		}
+
 		private static void Game_OnWndProc(WndEventArgs args) {
 			if (args.WParam == 'T' && !Game.IsChatOpen) {
 				_enabled = args.Msg == (uint) Utils.WindowsMessages.WM_KEYDOWN ? true : false;
@@ -35,16 +40,14 @@ namespace HpMpAbuse {
 			}
 		}
 
-		private static void Game_OnUpdate(EventArgs args) {
-
-			if (!Game.IsInGame || !_enabled || Game.IsPaused || !Utils.SleepCheck("delay"))
-				return;
-
-			var player = ObjectMgr.LocalPlayer;
-			if (player == null || player.Team == Team.Observer)
+		private static void Game_OnIngameUpdate(EventArgs args) {
+			if (Game.IsPaused || !_enabled || !Utils.SleepCheck("delay"))
 				return;
 
 			var hero = ObjectMgr.LocalHero;
+
+			if (hero == null || !hero.IsAlive)
+				return;
 
 			if (hero.Mana == hero.MaximumMana && hero.Health == hero.MaximumHealth)
 				return;
@@ -55,99 +58,197 @@ namespace HpMpAbuse {
 				return;
 			}
 
-			var arcaneBoots = hero.FindItem("item_arcane_boots") ?? hero.FindItem("item_guardian_greaves");
+			var arcaneBoots = hero.FindItem("item_arcane_boots");
+			var greaves = hero.FindItem("item_guardian_greaves");
 			var soulRing = hero.FindItem("item_soul_ring");
 			var bottle = hero.FindItem("item_bottle");
 			var stick = hero.FindItem("item_magic_stick") ?? hero.FindItem("item_magic_wand");
 			var powerTreads = hero.FindItem("item_power_treads");
-
-			var items = hero.Inventory.Items.ToList();
-
-			foreach (var item in items.Where(item => item.AbilityData.Any(x => (x.Name == "bonus_intellect" || x.Name == "bonus_strength" || x.Name == "bonus_all_stats" || x.Name == "bonus_health" || x.Name == "bonus_mana")))) {
-				if (arcaneBoots != null && item.Equals(arcaneBoots) && arcaneBoots.CanBeCasted())
-					continue;
-				if (stick != null && item.Equals(stick) && stick.CanBeCasted() && stick.CurrentCharges != 0)
-					continue;
-
-				hero.DropItem(item, hero.NetworkPosition);
-			}
-
-			if (arcaneBoots != null && arcaneBoots.CanBeCasted()) {
-				arcaneBoots.UseAbility();
-				hero.DropItem(arcaneBoots, hero.NetworkPosition);
-				Utils.Sleep(100, "delay");
-				return;
-			}
+			var meka = hero.FindItem("item_mekansm");
+			var urn = hero.FindItem("item_urn_of_shadows");
 
 			if (powerTreads != null && !_ptChanged) {
-				var sr = soulRing != null && soulRing.CanBeCasted();
-
+				//_lastPtState = ((Ensage.Items.PowerTreads) powerTreads).ActiveAttribute;
 				switch (((Ensage.Items.PowerTreads) powerTreads).ActiveAttribute) {
 					case Ensage.Attribute.Intelligence: // agi
-						if (sr) {
-							powerTreads.ToggleAbility(true);
-							soulRing.UseAbility();
-							powerTreads.ToggleAbility(true);
-							powerTreads.ToggleAbility(true);
-						}
-						_lastPtState = 0;
+						_lastPtState = Ensage.Attribute.Agility;
 					break;
 					case Ensage.Attribute.Strength:
-						if (sr) {
-							soulRing.UseAbility();
-						}
-						powerTreads.ToggleAbility(true);
-						powerTreads.ToggleAbility(true);
-						_lastPtState = 1;
+						_lastPtState = Ensage.Attribute.Strength;
 					break;
 					case Ensage.Attribute.Agility:  // int
-						if (sr) {
-							powerTreads.ToggleAbility(true);
-							powerTreads.ToggleAbility(true);
-							soulRing.UseAbility();
-							powerTreads.ToggleAbility(true);
-							powerTreads.ToggleAbility(true);
-						}
-						else {
-							powerTreads.ToggleAbility(true);
-						}
-						_lastPtState = 2;
+						_lastPtState = Ensage.Attribute.Intelligence;
 					break;
 				}
-				_ptChanged = true;
 			}
 
-			if (soulRing != null && soulRing.CanBeCasted() && powerTreads == null)
-				soulRing.UseAbility();
-				
-			if (bottle != null && bottle.CanBeCasted() && bottle.CurrentCharges != 0 && hero.Modifiers.All(x => x.Name != "modifier_bottle_regeneration"))
-				bottle.UseAbility();
+			if (meka != null && meka.CanBeCasted() && hero.Health != hero.MaximumHealth) {
+				ChangePt(powerTreads, Ensage.Attribute.Intelligence);
+				DropItems(bonusHealth, meka);
+				meka.UseAbility(true);
+			}
 
-			if (stick != null && stick.CanBeCasted() && stick.CurrentCharges != 0)
-				stick.UseAbility();
+			if (arcaneBoots != null && arcaneBoots.CanBeCasted() && hero.Mana != hero.MaximumMana) {
+				ChangePt(powerTreads, Ensage.Attribute.Agility);
+				DropItems(bonusMana, arcaneBoots);
+				arcaneBoots.UseAbility(true);
+			}
+
+			if (greaves != null && greaves.CanBeCasted()) {
+				ChangePt(powerTreads, Ensage.Attribute.Agility);
+				DropItems(bonusHealth.Concat(bonusMana), greaves);
+				greaves.UseAbility(true);
+			}
+
+			if (soulRing != null && soulRing.CanBeCasted()) {
+				ChangePt(powerTreads, Ensage.Attribute.Strength);
+				DropItems(bonusMana);
+				soulRing.UseAbility(true);
+			}
+
+			if (bottle != null && bottle.CanBeCasted() && bottle.CurrentCharges != 0 && hero.Modifiers.All(x => x.Name != "modifier_bottle_regeneration")) {
+				ChangePt(powerTreads, Ensage.Attribute.Agility);
+
+				if (hero.Health / hero.MaximumHealth < 0.9)
+					DropItems(bonusHealth);
+				if (hero.Mana / hero.MaximumMana < 0.9)
+					DropItems(bonusMana);
+
+				bottle.UseAbility(true);
+			}
+
+			if (stick != null && stick.CanBeCasted() && stick.CurrentCharges != 0) {
+				ChangePt(powerTreads, Ensage.Attribute.Agility);
+
+				if (hero.Health / hero.MaximumHealth < 0.9)
+					DropItems(bonusHealth, stick);
+				if (hero.Mana / hero.MaximumMana < 0.9)
+					DropItems(bonusMana, stick);
+
+				stick.UseAbility(true);
+			}
+
+			if (urn != null && urn.CanBeCasted() && urn.CurrentCharges != 0 && hero.Modifiers.All(x => x.Name != "modifier_item_urn_heal") && hero.Health / hero.MaximumHealth < 0.9) {
+				ChangePt(powerTreads, Ensage.Attribute.Agility);
+				DropItems(bonusHealth, urn);
+				urn.UseAbility(hero, true);
+			}
+
+			if (hero.Modifiers.Any(x => (x.Name == "modifier_item_urn_heal" || x.Name == "modifier_flask_healing"))) {
+				ChangePt(powerTreads, Ensage.Attribute.Agility);
+				DropItems(bonusHealth);
+			}
+
+			if (hero.Modifiers.Any(x => x.Name == "modifier_bottle_regeneration")) {
+				ChangePt(powerTreads, Ensage.Attribute.Agility);
+
+				if (hero.Health / hero.MaximumHealth < 0.9)
+					DropItems(bonusHealth);
+				if (hero.Mana / hero.MaximumMana < 0.9)
+					DropItems(bonusMana);
+			}
+
+			var allies = ObjectMgr.GetEntities<Hero>().Where(x => x.Distance2D(hero) <= 900 && x.IsAlive && x.Team == hero.Team);
+
+			foreach (var ally in allies) {
+				var allyArcaneBoots = ally.FindItem("item_arcane_boots");
+				var allyMeka = ally.FindItem("item_mekansm");
+				var allyGreaves = ally.FindItem("item_guardian_greaves");
+
+				if (allyArcaneBoots != null && allyArcaneBoots.AbilityState == AbilityState.Ready) {
+					ChangePt((Ensage.Items.PowerTreads) powerTreads, Ensage.Attribute.Agility);
+					DropItems(bonusMana);
+				}
+
+				if (allyMeka != null && allyMeka.AbilityState == AbilityState.Ready) {
+					ChangePt((Ensage.Items.PowerTreads) powerTreads, Ensage.Attribute.Agility);
+					DropItems(bonusHealth);
+				}
+
+				if (allyGreaves != null && allyGreaves.AbilityState == AbilityState.Ready) {
+					ChangePt((Ensage.Items.PowerTreads) powerTreads, Ensage.Attribute.Agility);
+					DropItems(bonusMana.Concat(bonusHealth));
+				}
+			}
 
 			Utils.Sleep(250, "delay");
 		}
 
-		private static void PickUpItems() {
-
+		private static void DropItems(IEnumerable<string> drop, Ensage.Item ignore = null) {
 			var hero = ObjectMgr.LocalHero;
+			var items = hero.Inventory.Items.ToList();
+
+			foreach (var item in items.Where(item => !item.Equals(ignore) && item.AbilityData.Any(x => drop.Any(x.Name.Contains)))) {
+				hero.DropItem(item, hero.NetworkPosition, true);
+			}
+		}
+
+		private static void ChangePt(Ensage.Item pt, Ensage.Attribute atrb) {  // okay this shit function is really bad
+			if (pt == null)
+				return;
+
+			//var ptNow = (int) ((Ensage.Items.PowerTreads) pt).ActiveAttribute + 1;
+			//var ptTo = (int) atrb + 1;
+
+			var ptNow = 0;
+			var ptTo = 0;
+
+			//some random fixes
+			switch (((Ensage.Items.PowerTreads) pt).ActiveAttribute) {
+				case Ensage.Attribute.Intelligence: // agi
+					ptNow = 3;
+				break;
+				case Ensage.Attribute.Strength:
+					ptNow = 1;
+				break;
+				case Ensage.Attribute.Agility:  // int
+					ptNow = 2;
+				break;
+			}
+
+			// more fixes
+			switch (atrb) {
+				case Ensage.Attribute.Intelligence:
+					ptTo = 2;
+				break;
+				case Ensage.Attribute.Strength:
+					ptTo = 1;
+				break;
+				case Ensage.Attribute.Agility:
+					ptTo = 3;
+				break;
+			}
+
+			if (ptNow == ptTo)
+				return;
+			
+			var change = ptTo - ptNow % 3;
+
+			if (ptNow == 2 && ptTo == 1) // another fix
+				change = 2;
+
+			for (var i = 0; i < change; i++)
+				pt.ToggleAbility(true);
+
+			_ptChanged = true;
+		}
+
+		private static void PickUpItems() {
+			var hero = ObjectMgr.LocalHero;
+
+			if (hero == null)
+				return;
 
 			var droppedItems = ObjectMgr.GetEntities<PhysicalItem>().Where(x => x.Distance2D(hero) < 250).ToList();
 
-			foreach (var item in droppedItems) {
-				hero.PickUpItem(item);
-			}
+			for (var i = 0; i < droppedItems.Count; i++)
+				hero.PickUpItem(droppedItems[i], i != 0);
+			
+			if (!_ptChanged)
+				return;
 
-			var powerTreads = hero.FindItem("item_power_treads");
-
-			if (powerTreads != null && _ptChanged) {
-				for (var i = 0; i < _lastPtState; i++) {
-					powerTreads.ToggleAbility(true);
-				}
-				_ptChanged = false;
-			}
-
+			ChangePt(hero.FindItem("item_power_treads"), _lastPtState);
+			_ptChanged = false;
 		}
 
 	}
