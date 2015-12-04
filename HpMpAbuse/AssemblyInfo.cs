29eacb104eeac01ea7f8cc7ac427af0971272b5c
IdcNoob <zajabli@gmail.com>
2015-12-04 03:16:41 +0200

    PT Switcher added

--- HpMpAbuse/AssemblyInfo.cs
+++ HpMpAbuse/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HpMpAbuse")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Yolo")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Basement")]
 [assembly: AssemblyProduct("HpMpAbuse")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright ©  2045")]
+[assembly: AssemblyTrademark("Who cares")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
