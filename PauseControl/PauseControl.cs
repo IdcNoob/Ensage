6ede3bdf0a59775061379361bf0fa19d849531b2
IdcNoob <zajabli@gmail.com>
2017-05-29 17:25:57 +0300

    Update

---
+++ PauseControl/PauseControl.cs
@@ -0,0 +1,93 @@
+﻿namespace PauseControl
+{
+    using System.ComponentModel.Composition;
+
+    using Ensage;
+    using Ensage.SDK.Handlers;
+    using Ensage.SDK.Helpers;
+    using Ensage.SDK.Service;
+    using Ensage.SDK.Service.Metadata;
+
+    [ExportPlugin("PauseControl", StartupMode.Auto, "IdcNoob")]
+    internal class PauseControl : Plugin
+    {
+        private static Team heroTeam;
+
+        private Settings settings;
+
+        private IUpdateHandler updateHandler;
+
+        [ImportingConstructor]
+        public PauseControl([Import] IServiceContext context)
+        {
+            heroTeam = context.Owner.Team;
+        }
+
+        protected override void OnActivate()
+        {
+            settings = new Settings();
+
+            ObjectManager.OnRemoveEntity += OnRemoveEntity;
+            Entity.OnInt32PropertyChange += OnInt32PropertyChange;
+            updateHandler = UpdateManager.Subscribe(OnUpdate, 5000, false);
+        }
+
+        protected override void OnDeactivate()
+        {
+            ObjectManager.OnRemoveEntity -= OnRemoveEntity;
+            Entity.OnInt32PropertyChange -= OnInt32PropertyChange;
+            UpdateManager.Unsubscribe(OnUpdate);
+
+            settings.Dispose();
+        }
+
+        private void OnInt32PropertyChange(Entity sender, Int32PropertyChangeEventArgs args)
+        {
+            if (sender.ClassId != ClassId.CDOTAGamerulesProxy || args.PropertyName != "m_iPauseTeam")
+            {
+                return;
+            }
+
+            var pauseTeam = (Team)args.NewValue;
+            if (pauseTeam == heroTeam || pauseTeam == Team.Observer)
+            {
+                updateHandler.IsEnabled = false;
+                return;
+            }
+
+            updateHandler.IsEnabled = settings.Unpause;
+        }
+
+        private void OnRemoveEntity(EntityEventArgs args)
+        {
+            if (!settings.Pause)
+            {
+                return;
+            }
+
+            var player = args.Entity as Player;
+            if (player != null && player.Team == heroTeam)
+            {
+                if (!Game.IsPaused)
+                {
+                    Network.PauseGame();
+                }
+                else if (updateHandler.IsEnabled)
+                {
+                    updateHandler.IsEnabled = false;
+                }
+            }
+        }
+
+        private void OnUpdate()
+        {
+            if (Game.IsPaused)
+            {
+                Network.PauseGame();
+                return;
+            }
+
+            updateHandler.IsEnabled = false;
+        }
+    }
+}
---
+++ PauseControl/Settings.cs
@@ -0,0 +1,31 @@
+﻿namespace PauseControl
+{
+    using System;
+
+    using Ensage.SDK.Menu;
+
+    internal class Settings : IDisposable
+    {
+        private readonly MenuFactory factory;
+
+        public Settings()
+        {
+            factory = MenuFactory.Create("Pause Control");
+
+            Pause = factory.Item("Auto pause", true);
+            Pause.Item.SetTooltip("Auto pause when ally is disconnected");
+
+            Unpause = factory.Item("Force unpause", true);
+            Unpause.Item.SetTooltip("Force unpause if pause is set by enemies");
+        }
+
+        public MenuItem<bool> Pause { get; }
+
+        public MenuItem<bool> Unpause { get; }
+
+        public void Dispose()
+        {
+            factory?.Dispose();
+        }
+    }
+}
