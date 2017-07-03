d0074624953725d3917a98be8d759177f7221c13
frjabur <frjabur@gmail.com>
2017-07-03 15:45:25 -0300

    Update Shadowraze.cs

--- Evader/EvadableAbilities/Heroes/ShadowFiend/Shadowraze.cs
+++ Evader/EvadableAbilities/Heroes/ShadowFiend/Shadowraze.cs
@@ -23,7 +23,7 @@
         }
         protected override float GetRadius()
         {
-            return base.GetRadius() + 300;
+            return base.GetRadius() + 100;
         }
         public override void Check()
         {
