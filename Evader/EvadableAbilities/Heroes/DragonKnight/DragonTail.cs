61713217edf16d22002c8de974a38b971c1c11c1
IdcNoob <zajabli@gmail.com>
2017-09-21 11:13:11 +0300

    improved dk stun

--- Evader/EvadableAbilities/Heroes/DragonKnight/DragonTail.cs
+++ Evader/EvadableAbilities/Heroes/DragonKnight/DragonTail.cs
@@ -1,6 +1,7 @@
 ﻿namespace Evader.EvadableAbilities.Heroes.DragonKnight
 {
-    using Base;
+    using System.Linq;
+
     using Base.Interfaces;
 
     using Ensage;
@@ -9,32 +10,45 @@
 
     using static Data.AbilityNames;
 
-    internal class DragonTail : EvadableAbility, IModifier
+    using Projectile = Base.Projectile;
+
+    internal class DragonTail : Projectile, IModifier
     {
+        private readonly float speed;
+
         public DragonTail(Ability ability)
             : base(ability)
         {
             Modifier = new EvadableModifier(HeroTeam, EvadableModifier.GetHeroType.ModifierSource);
 
+            CounterAbilities.Add(PhaseShift);
+            CounterAbilities.Add(Eul);
+            CounterAbilities.Add(SleightOfFist);
+            CounterAbilities.Add(Manta);
+            CounterAbilities.Add(BallLightning);
+            CounterAbilities.AddRange(VsDisable);
+            CounterAbilities.AddRange(VsDamage);
+            CounterAbilities.AddRange(VsPhys);
+            CounterAbilities.AddRange(Invis);
+            CounterAbilities.Add(Armlet);
+            CounterAbilities.Add(Bloodstone);
+            CounterAbilities.Add(Lotus);
+            CounterAbilities.Add(HurricanePike);
+            CounterAbilities.Remove("abaddon_aphotic_shield");
+
             Modifier.AllyCounterAbilities.Add(Enrage);
             Modifier.AllyCounterAbilities.AddRange(AllyShields);
             Modifier.AllyCounterAbilities.AddRange(Invul);
             Modifier.AllyCounterAbilities.AddRange(VsPhys);
+
+            speed = ability.AbilitySpecialData.First(x => x.Name == "projectile_speed").Value;
         }
 
         public EvadableModifier Modifier { get; }
 
-        public override void Check()
+        public override float GetProjectileSpeed()
         {
-        }
-
-        public override void Draw()
-        {
-        }
-
-        public override float GetRemainingTime(Hero hero = null)
-        {
-            return 0;
+            return speed;
         }
     }
 }
